4cbb1f326ef6f70b0927fbdf5a6b322fdf3947a2
AntiKrzy <antek.rysunek@gmail.com>
2025-06-22 17:43:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('orderForm').addEventListener('submit', function(e) {
   e.preventDefault();
   const formData = new FormData(this);
 
-  fetch('YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL', {
+  fetch('https://script.google.com/macros/s/AKfycbyMS3be7NYQWno0vSWYG82WpCQDUwWcjCYM_dl0dQutk5BhueZvMzV0y-pLgQ0nIW-C2w/exec', {
     method: 'POST',
     body: formData
   })
